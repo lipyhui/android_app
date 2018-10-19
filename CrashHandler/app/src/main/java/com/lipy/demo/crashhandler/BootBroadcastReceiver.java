3768c3972b4ba1823ffecaea6a25a88d946f37e0
liph <penghui.li@kawakp.com>
2018-10-19 10:16:36 +0800

    添加开机启动

---
+++ CrashHandler/app/src/main/java/com/lipy/demo/crashhandler/BootBroadcastReceiver.java
@@ -0,0 +1,31 @@
+package com.lipy.demo.crashhandler;
+
+/**
+ * 创建人: lipy
+ * 创建时间: 2018/4/23
+ * 修改人:qicheng.deng
+ * 修改时间:2018/4/23
+ * 修改内容:
+ *
+ * 功能描述:开机自启
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		final String action_boot ="android.intent.action.BOOT_COMPLETED";
+
+		if (intent.getAction().equals(action_boot)){
+
+			Intent intent2 = new Intent(context, MainActivity.class);
+			intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(intent2);
+		}
+	}
+}
+
+
